430b1f2a36cbafdc7339881856626075e3a3da45
Suvanga <suvanga.dhz@gmail.com>
2025-03-08 16:55:03 -0500

    Base of Mongo db is setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import connectDB from "./services/mongodb.js";
 const app = express();
 
-const PORT = 4001;
+dotenv.config();
+
+const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {  
-console.log(`Server is running on port ${PORT}`);   
+console.log(`Server is running on port ${PORT}`);
+connectDB()   
 })
