881f6a3c53eca72031b108980ad460a0e37f54e9
Kritan Aryal <147074025+KAryal10@users.noreply.github.com>
2025-03-14 14:35:57 -0400

    Update userRoute.js

--- backend/routes/admin/User/userRoute.js
+++ backend/routes/admin/User/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import UserController from '../../userController.js';
+import UserController from '../../../Controller/userController.js';
 
 const router = express.Router();
 
